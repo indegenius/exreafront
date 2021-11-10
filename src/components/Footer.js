6272e5011f1808f64acf5bde001154b55901e12f
indieurbanmusictv@gmail.com <indieurbanmusictv@gmail.com>
2021-11-10 03:43:56 -0500

    frontend

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 function Footer(props) {
-    return <h1>Footer</h1>;
+    return <h5>site by <a href="http://iyahtv.com"><img src="https://howtobeafamousrapper.com/wp-content/uploads/2014/12/iyahlogo.png" alt="iyah fine arts logo" /></a>IYAH</h5>;
   }
   
   export default Footer;
